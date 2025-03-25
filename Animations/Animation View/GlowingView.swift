a911f5a543ea105f805ab275716695dd87acf8f8
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 10:47:38 +0500

    A new file gwloing view s acreated along witht he code structure is added

---
+++ Animations/Animation View/GlowingView.swift
@@ -0,0 +1,24 @@
+//
+//  GlowingView.swift
+//  Animations
+//
+//  Created by Elexoft on 25/03/2025.
+//
+
+import SwiftUI
+
+struct GlowingView: View {
+    // MARK: - PROEPRTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    GlowingView()
+}
