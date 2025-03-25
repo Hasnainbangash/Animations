f5db8b78c4f7ae4a29c56d9c00f3ae6ce01a65bd
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 11:06:15 +0500

    Added te rounded rectangle and the image of the heart ot he view

--- Animations/Animation View/GlowingView.swift
+++ Animations/Animation View/GlowingView.swift
@@ -15,10 +15,23 @@ struct GlowingView: View {
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
-            Circle()
-                .frame(width: 100)
-                .foregroundColor(.yellow)
-                .glow()
+            
+            VStack(spacing: 20) {
+                Circle()
+                    .frame(width: 100)
+                    .foregroundColor(.yellow)
+                    .glow()
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.blue)
+                    .glow()
+                
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.red)
+                    .font(.system(size: 100))
+                    .glow()
+            }
         } //: ZSTACK
     }
 }
@@ -30,6 +43,8 @@ struct Glow: ViewModifier {
         ZStack {
             content
                 .blur(radius: 15)
+            
+            content
         } //: ZSTACK
     }
 }
