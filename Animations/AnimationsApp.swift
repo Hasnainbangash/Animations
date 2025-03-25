6bb096792e751cfcd1cf239baa824ccc63f27373
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 10:04:32 +0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Elexoft on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
