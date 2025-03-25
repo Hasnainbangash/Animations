ed526452593633b19f738d67e4cccb1a80bb9c3d
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 12:10:54 +0500

    Images sets are added to the assets and the imagepreview file is made along with the code structure comments are added to the file

---
+++ Animations/Animation View/ImagePreview.swift
@@ -0,0 +1,24 @@
+//
+//  ImagePreview.swift
+//  Animations
+//
+//  Created by Elexoft on 25/03/2025.
+//
+
+import SwiftUI
+
+struct ImagePreview: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    ImagePreview()
+}
