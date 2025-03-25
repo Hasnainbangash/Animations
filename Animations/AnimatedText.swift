0bd1f7831db19a29c2248fb60a92aaba42ab51b3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 10:14:11 +0500

    Added the animation of the linear and a bool propetty for the movegradient is crated

--- Animations/AnimatedText.swift
+++ Animations/AnimatedText.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct AnimatedText: View {
+    // MARK: - PROPERTIES
+    
+    @State private var moveGradient = true
+    
+    // MARK: - BODY
+    
     var body: some View {
         Rectangle()
             .overlay {
                 LinearGradient(colors: [.clear, .white, .clear], startPoint: .leading, endPoint: .trailing)
             }
+            .animation(.linear(duration: 2), value: moveGradient)
+            .onAppear {
+                self.moveGradient.toggle()
+            }
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     AnimatedText()
