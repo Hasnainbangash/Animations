f5535810deb2f0334a170362be8c2a1045a18cb9
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 10:21:22 +0500

    Some acooments are added tje code

--- Animations/AnimatedText.swift
+++ Animations/AnimatedText.swift
@@ -18,10 +18,11 @@ struct AnimatedText: View {
         Rectangle()
             .overlay {
                 LinearGradient(colors: [.clear, .white, .clear], startPoint: .leading, endPoint: .trailing)
+                    // Here the offset will move the colors in the horizontal to 100 points forward from the origin
                     .offset(x: moveGradient ? 0 : 100)
             }
             // This linear animatons means that animation happens at a constant speed without any acceleration or deceleration.
-            .animation(.linear(duration: 2), value: moveGradient)
+            .animation(.linear(duration: 2).repeatForever(autoreverses: false), value: moveGradient)
             .onAppear {
                 self.moveGradient.toggle()
             }
