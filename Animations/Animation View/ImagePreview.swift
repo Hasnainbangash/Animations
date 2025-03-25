d7161b28faf38e9a5db838e9f663afe2dfbb1f8c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 12:37:33 +0500

    Image preview animations wokr done

--- Animations/Animation View/ImagePreview.swift
+++ Animations/Animation View/ImagePreview.swift
@@ -10,28 +10,49 @@ import SwiftUI
 struct ImagePreview: View {
     // MARK: - PROPERTIES
     
+    @Namespace private var previewSmoothly
+    @State private var preview: Bool = false
+    @State private var selectedImage: String?
+    
     // MARK: - FUNCTIONS
     
-    func image(_ image: String) -> some View {
-        Image(image)
+    func image(_ imageName: String) -> some View {
+        Image(imageName)
             .resizable()
+            .matchedGeometryEffect(id: imageName, in: previewSmoothly)
+            .zIndex(selectedImage == imageName ? 1 : 0)
+            .onTapGesture {
+                withAnimation(Animation.easeInOut(duration: 0.3)) {
+                    selectedImage = imageName
+                    preview.toggle()
+                }
+            }
     }
     
     // MARK: - BODY
     
     var body: some View {
-        Grid {
-            GridRow {
-                image("image1")
-                image("image2")
-            } //: GRID ROW
-            
-            GridRow {
-                image("image3")
-                image("image4")
-            } //: GRID ROW
-        } //: GRID
-        .padding(10)
+        ZStack {
+            if preview {
+                if let selectedImage = selectedImage {
+                    image(selectedImage)
+                        .ignoresSafeArea()
+                }
+            } else {
+                Grid {
+                    GridRow {
+                        image("image1")
+                        image("image2")
+                    } //: GRID ROW
+                    
+                    GridRow {
+                        image("image3")
+                        image("image4")
+                    } //: GRID ROW
+                } //: GRID
+                .padding(10)
+            }
+        } //: ZSTACK
     }
 }
 
