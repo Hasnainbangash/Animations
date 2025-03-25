f1373dcd945a34758ab21dd9366261844f2eaa0b
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 10:18:43 +0500

    Added the offset to the linear gradient ot move the colors from the origin point to the forward 100 points

--- Animations/AnimatedText.swift
+++ Animations/AnimatedText.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AnimatedText: View {
     // MARK: - PROPERTIES
     
-    @State private var moveGradient = true
+    @State private var moveGradient: Bool = true
     
     // MARK: - BODY
     
@@ -18,7 +18,9 @@ struct AnimatedText: View {
         Rectangle()
             .overlay {
                 LinearGradient(colors: [.clear, .white, .clear], startPoint: .leading, endPoint: .trailing)
+                    .offset(x: moveGradient ? 0 : 100)
             }
+            // This linear animatons means that animation happens at a constant speed without any acceleration or deceleration.
             .animation(.linear(duration: 2), value: moveGradient)
             .onAppear {
                 self.moveGradient.toggle()
