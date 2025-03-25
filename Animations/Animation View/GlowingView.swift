263f8d470a5bff5ea7e78f0beff58b372e561888
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 10:54:50 +0500

    Added a modifier and an extension and apply the extension to the view modifer

--- Animations/Animation View/GlowingView.swift
+++ Animations/Animation View/GlowingView.swift
@@ -13,9 +13,32 @@ struct GlowingView: View {
     // MARK: - BODY
     
     var body: some View {
-        Circle()
-            .frame(width: 100)
-            .foregroundColor(.yellow)
+        ZStack {
+            Color.black.ignoresSafeArea()
+            Circle()
+                .frame(width: 100)
+                .foregroundColor(.yellow)
+                .glow()
+        } //: ZSTACK
+    }
+}
+
+// MARK: - MODIFIER
+
+struct Glow: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+                .blur(radius: 15)
+        } //: ZSTACK
+    }
+}
+
+// MARK: - EXTENSION
+
+extension View {
+    func glow() -> some View {
+        modifier(Glow())
     }
 }
 
