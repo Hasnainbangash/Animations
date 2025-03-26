3f8f557d1bc420a328fa2afe30f0e18e92fa021f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-26 10:36:07 +0500

    A new loading view is created along with the body structure comments

---
+++ Animations/Animation View/LoadingView.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingView.swift
+//  Animations
+//
+//  Created by Elexoft on 26/03/2025.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    LoadingView()
+}
