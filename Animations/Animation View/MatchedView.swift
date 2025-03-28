05a5b822988bd19384b743a35b8f90a48c2083f7
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-28 10:45:02 +0500

    matched geomerty is added to the along with the comments

--- Animations/Animation View/MatchedView.swift
+++ Animations/Animation View/MatchedView.swift
@@ -19,9 +19,14 @@ struct MatchedView: View {
         ZStack {
             if !show {
                 Text("SwiftUI")
+                    .font(.largeTitle.weight(.bold))
+                    // The id is just so that we can identify between the two shared elements across multiple views and the namespace is pretty much always the same. It's important to understand that when you are using matched geometry you have to keep the same integrity like the size should be same for example font size, background etc of the element before the modifier and ideally you almost have the same style between the two to have a perfect transition.
+                    .matchedGeometryEffect(id: "title", in: namespace)
                     .frame(maxWidth: .infinity, alignment: .leading)
             } else {
                 Text("SwiftUI")
+                    .font(.largeTitle.weight(.bold))
+                    .matchedGeometryEffect(id: "title", in: namespace)
                     .frame(maxWidth: .infinity, alignment: .trailing)
             }
         } //: ZSTACK
