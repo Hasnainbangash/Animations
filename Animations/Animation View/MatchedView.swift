9f65667050e93df955d70159ea28e3518b724dc3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-28 14:42:05 +0500

    Embedded the expand view in the scroll view

--- Animations/Animation View/MatchedView.swift
+++ Animations/Animation View/MatchedView.swift
@@ -56,39 +56,41 @@ struct MatchedView: View {
                 .frame(height: 300)
                 .padding(20)
             } else {
-                VStack(alignment: .leading, spacing: 12) {
-                    Spacer()
-                    
-                    Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text", in: namespace)
-                    
-                    Text("20 sections - 3 hours".uppercased())
-                        .font(.footnote.bold())
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    
-                    Text("SwiftUI")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                } //: VSTACK
-                .padding(20)
-                .foregroundStyle(.black)
-                .background(
-                    Image("Illustration1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .matchedGeometryEffect(id: "image", in: namespace)
-                )
-                .background(
-                    Image("Background 5")
-                        .resizable()
-                        .matchedGeometryEffect(id: "background", in: namespace)
-                )
-                .mask {
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .matchedGeometryEffect(id: "mask", in: namespace)
-                }
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 12) {
+                        Spacer()
+                        
+                        Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                            .font(.footnote)
+                            .matchedGeometryEffect(id: "text", in: namespace)
+                        
+                        Text("20 sections - 3 hours".uppercased())
+                            .font(.footnote.bold())
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        
+                        Text("SwiftUI")
+                            .font(.largeTitle.weight(.bold))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    } //: VSTACK
+                    .padding(20)
+                    .foregroundStyle(.black)
+                    .background(
+                        Image("Illustration1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .matchedGeometryEffect(id: "image", in: namespace)
+                    )
+                    .background(
+                        Image("Background 5")
+                            .resizable()
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
+                    }
+                } //: SCROLL VIEW
             }
         } //: ZSTACK
         .onTapGesture {
