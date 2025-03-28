fcc37bbb75733fcd13429615d833436443ce396c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-28 10:49:01 +0500

    Some more design work done by using the vstack

--- Animations/Animation View/MatchedView.swift
+++ Animations/Animation View/MatchedView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct MatchedView: View {
     // MARK: - PROPERTIES
     
+    // The id is just so that we can identify between the two shared elements across multiple views and the namespace is pretty much always the same. It's important to understand that when you are using matched geometry you have to keep the same integrity like the size should be same for example font size, background etc of the element before the modifier and ideally you almost have the same style between the two to have a perfect transition.
+    
     @Namespace var namespace
     @State var show: Bool = false
     
@@ -18,16 +20,25 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                Text("SwiftUI")
-                    .font(.largeTitle.weight(.bold))
-                    // The id is just so that we can identify between the two shared elements across multiple views and the namespace is pretty much always the same. It's important to understand that when you are using matched geometry you have to keep the same integrity like the size should be same for example font size, background etc of the element before the modifier and ideally you almost have the same style between the two to have a perfect transition.
-                    .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                VStack {
+                    Text("SwiftUI")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.bold())
+                } //: VSTACK
             } else {
-                Text("SwiftUI")
-                    .font(.largeTitle.weight(.bold))
-                    .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
+                VStack {
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.bold())
+                    
+                    Text("SwiftUI")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                } //: VSTACK
             }
         } //: ZSTACK
         .onTapGesture {
